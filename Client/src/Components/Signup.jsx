f3ce22115559f881fd73e2e3ff41a65fd678f909
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-04-04 15:36:59 +0530

    Made changes given in the codium AI agent feedback

--- Client/src/Components/Signup.jsx
+++ Client/src/Components/Signup.jsx
@@ -2,70 +2,95 @@ import './Signup.css'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-function Signup(){
+function Signup() {
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [signupError, setSignupError] = useState('');
 
-  return(
-    <>
-    <nav>
-        <div className='navbar'>
-            <h1 className='title'>Serenity Steps</h1>
-        </div>
-    </nav>
-    <div className="form-container">
-      <form className="signupform">
-      <label>First Name:</label>
-        <input 
-          type="text"  
-          required 
-        />
-        <br></br>
-
-        <label>Last Name:</label>
-        <input 
-          type="text"  
-          required 
-        />
-        <br></br>
-
-        <label>Contact Number:</label>
-        <input 
-          type="tel"  
-          required 
-        />
-        <br></br>
-
-        <label>Email:</label>
-        <input 
-          type="email"  
-          required 
-        />
-        <br></br>
-
-        <label>Create Username:</label>
-        <input 
-          type="text"  
-          required 
-        />
-        <br></br>
-
-        <label>Create Password:</label>
-        <input
-          type="password"
-          required
-          minLength={6}
-        />
-        <br />
-        <button type="submit" className="signup-btn">SIGNUP</button>
-      </form>
-    </div>
-    </>
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
       
-  );
+      navigate('/'); 
+    } catch (error) {
+      setSignupError(error.message);
+    }
+  };
 
+  return (
+    <>
+      <nav>
+        <div className='navbar'>
+          <h1 className='title'>Serenity Steps</h1>
+        </div>
+      </nav>
+      <div className="form-container">
+        <form className="signupform" onSubmit={handleFormSubmit}>
+          <label>First Name:</label>
+          <input 
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+          <br />
+
+          <label>Last Name:</label>
+          <input 
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+          <br />
+
+          <label>Contact Number:</label>
+          <input 
+            type="tel"
+            value={contactNumber}
+            onChange={(e) => setContactNumber(e.target.value)}
+            required
+          />
+          <br />
+
+          <label>Email:</label>
+          <input 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <br />
+
+          <label>Create Username:</label>
+          <input 
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <br />
+
+          <label>Create Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength={6}
+          />
+          <br />
+
+          <button type="submit" className="signup-btn">SIGNUP</button>
+        </form>
+      </div>
+    </>
+  );
 }
+
 export default Signup;
