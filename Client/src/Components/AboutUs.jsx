055599cc3138b5ecb6d66cd1ba4d3b02926295ee
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-04-10 15:19:45 +0530

    Made changes suggested by Codium AI agent

--- Client/src/Components/AboutUs.jsx
+++ Client/src/Components/AboutUs.jsx
@@ -11,12 +11,11 @@ function AboutUs(){
                     <Link to="/Login"><button className='loginbtn'>Login</button></Link>
                 </div>  
             </nav>
-            <div className='AboutUs-container'>
+            <div className='Aboutus-container'>
                 <div className='au-container'>
-                    <h1 className='info'>Serenity Steps is a integrated wellness platform that fosters the well-being by connecting meditation, mental health, physical fitness, and diet planning.It will provide the users with individualised advice and resources to help them maintain a healthy, balanced lifestyle.</h1>
+                    <div><h1 className='info'>Serenity Steps is a integrated wellness platform that fosters the well-being by connecting meditation, mental health, physical fitness, and diet planning.It will provide the users with individualised advice and resources to help them maintain a healthy, balanced lifestyle.</h1></div>
                 </div>
             </div>
-
         </>
     )
 }
